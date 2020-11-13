3f9eda5efa326faa8acd84ad212b5ff7dd3db052
ESSID Wadii <37578326+wadiiexa@users.noreply.github.com>
2020-11-13 14:40:38 +0100

    Update App.java

--- src/main/java/fr/imiddleware/projets/App.java
+++ src/main/java/fr/imiddleware/projets/App.java
@@ -9,6 +9,6 @@ public class App
    
     public static void main( String[] args )
     {
-        System.out.println( "my message" +  System.getProperty("message"));
+        System.out.println( "my message" + System.getenv("message");
     }
 }
